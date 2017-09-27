7bf2fe95812a5dbfde114e6d22d6f4bdb9df9022
yuralala8 <yc1435@nyu.edu>
2017-09-27 10:51:52 -0400

    fixed friendship

--- app/controllers/api/v1/friendships_controller.rb
+++ app/controllers/api/v1/friendships_controller.rb
@@ -3,9 +3,10 @@ class Api::V1::FriendshipsController < ApplicationController
   
   def pending_requests
   	@friendships = Friendship.select{|relation| relation.status == "pending"}
-    @friendships.select{|f| f.friend_id.to_s == params[:id]}
+    @pending_received = @friendships.select{|f| f.friend_id.to_s == params[:id]}
+    @pending_sent = @friendships.select{|f| f.user_id.to_s == params[:id]}
 
-  	render json: @friendships
+  	render json: {pending_received: @pending_received, pending_sent: @pending_sent}
   end
 
 
@@ -25,7 +26,6 @@ class Api::V1::FriendshipsController < ApplicationController
     @friends = Friendship.select{|friend| friend.status == "complete" && friend.user_id.to_s == params[:id]}
     my_friends = @friends.map{|relation| relation.friend_id}
     friends_list = my_friends.map{|num| User.all.find{|user| user.id == num}.username}
-
     render json: friends_list
   end
 
@@ -33,7 +33,7 @@ class Api::V1::FriendshipsController < ApplicationController
     @friendship = Friendship.new(user_id: params[:currentUser], friend_id: params[:friendId], status: "complete")
     @friendship.save
 
-    change_status_of = Friendship.find{|mutual| mutual.user_id == params[:friendId]}
+    change_status_of = Friendship.find_by(user_id: params[:friendId], friend_id: params[:currentUser])
     change_status_of.status = "complete"
     change_status_of.save
   
